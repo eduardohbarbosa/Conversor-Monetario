595e0c982def0ec1b9a1a9a096d561f3ec1f0de8
EDUARDO HENRIQUE <eduhenrique5478@gmail.com>
2021-09-17 17:50:54 -0300

    update 17-09
    Criação do HTML completo
    Criação do JS completo
    Criação do CSS incompleto

---
+++ script.js
@@ -0,0 +1,60 @@
+function converter(){
+    var moeda = Number(document.querySelector('#moeda1').value)
+    var unidade1 = document.querySelector('#unidade1').value
+    var unidade2 = document.querySelector('#unidade2').value
+    var resultado = document.querySelector('#resultado')
+
+    if(unidade1 == "reais"){
+        if(unidade2 == "dolar"){
+            var dolar = moeda * 5.29
+            resultado.innerHTML = `R$${moeda.toFixed(2).replace(".",",")} é igual à US$${dolar.toFixed(2).replace(".",",")}`
+        }else if(unidade2 == "euro"){
+            var euro = moeda * 6.20
+            resultado.innerHTML = `R$${moeda.toFixed(2).replace(".",",")} é igual à &euro;${euro.toFixed(2).replace(".",",")}`
+        }else if(unidade2 == "bitcoin"){
+            var bitcoin = moeda * 249486.71
+            resultado.innerHTML = `R$${moeda.toFixed(2).replace(".",",")} é igual à &#8383;${bitcoin.toFixed(2).replace(".",",")}`
+        }else{
+            resultado.innerHTML = `R$${moeda.toFixed(2).replace(".",",")} é igual à R$${moeda.toFixed(2).replace(".",",")}`
+        }
+    }else if(unidade1 == "dolar" ){
+        if(unidade2 == "reais"){
+            var reais = moeda / 5.29
+            resultado.innerHTML = `US$${moeda.toFixed(2).replace(".",",")} é igual à R$${reais.toFixed(2).replace(".",",")}`
+        }else if(unidade2 == "euro"){
+            var euro = moeda / 1.17
+            resultado.innerHTML = `US$${moeda.toFixed(2).replace(".",",")} é igual à &euro;${euro.toFixed(2).replace(".",",")}`
+        }else if(unidade2 == "bitcoin"){
+            var bitcoin = moeda / 47505.20
+            resultado.innerHTML = `US$${moeda.toFixed(2).replace(".",",")} é igual à &#8383;${bitcoin.toFixed(6).replace(".",",")}`
+        }else{
+            resultado.innerHTML = `US$${moeda.toFixed(2).replace(".",",")} é igual à US$${moeda.toFixed(2).replace(".",",")}`
+        }
+    }else if(unidade1 == "euro"){
+        if(unidade2 == "reais"){
+            var reais = moeda / 6.20
+            resultado.innerHTML = `&euro;${moeda.toFixed(2).replace(".",",")} é igual à R$${reais.toFixed(2).replace(".",",")}`
+        }else if(unidade2 == "dolar"){
+            var euro = moeda * 1.17
+            resultado.innerHTML = `&euro;${moeda.toFixed(2).replace(".",",")} é igual à US$${euro.toFixed(2).replace(".",",")}`
+        }else if(unidade2 == "bitcoin"){
+            var bitcoin = moeda / 40403.11
+            resultado.innerHTML = `&euro;${moeda.toFixed(2).replace(".",",")} é igual à &#8383;${bitcoin.toFixed(6).replace(".",",")}`
+        }else{
+            resultado.innerHTML = `&euro;${moeda.toFixed(2).replace(".",",")} é igual à &euro;${moeda.toFixed(2).replace(".",",")}`
+        }
+    }else if(unidade1 == "bitcoin"){
+        if(unidade2 == "reais"){
+            var reais = moeda / 249486.71
+            resultado.innerHTML = `&#8383;${moeda.toFixed(2).replace(".",",")} é igual à R$${reais.toFixed(6).replace(".",",")}`
+        }else if(unidade2 == "dolar"){
+            var dolar = moeda * 47505.20
+            resultado.innerHTML = `&#8383;${moeda.toFixed(2).replace(".",",")} é igual à US$${dolar.toFixed(2).replace(".",",")}`
+        }else if(unidade2 == "euro"){
+            var bitcoin = moeda * 40403.11
+            resultado.innerHTML = `&#8383;${moeda.toFixed(2).replace(".",",")} é igual à &euro;${bitcoin.toFixed(2).replace(".",",")}`
+        }else{
+            resultado.innerHTML = `&#8383;${moeda.toFixed(2).replace(".",",")} é igual à &#8383;${moeda.toFixed(2).replace(".",",")}`
+        }
+    }
+}
